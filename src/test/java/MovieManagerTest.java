8dd5def0178150ec5926feb0010659f57a175ee6
Kirill Igorevich <k.beketoff@yandex.ru>
2023-03-19 18:40:18 +0300

    First commit

---
+++ src/test/java/MovieManagerTest.java
@@ -0,0 +1,66 @@
+import org.example.MovieManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class MovieManagerTest {
+    @Test
+    public void test() {
+
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("film 1");
+        manager.addMovie("film 2");
+        manager.addMovie("film 3");
+
+        String[] expected = {"film 1", "film 2", "film 3"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFor1() {
+
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("film 1");
+
+
+        String[] expected = {"film 1"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFor0() {
+
+        MovieManager manager = new MovieManager();
+
+
+        String[] expected = {};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLast() {
+
+
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("film 1");
+        manager.addMovie("film 2");
+        manager.addMovie("film 3");
+        manager.addMovie("film 4");
+        manager.addMovie("film 5");
+
+
+        int expected = 1;
+        int actual = 1;
+        Assertions.assertEquals(expected, actual);
+
+
+    }
+}
+
+
